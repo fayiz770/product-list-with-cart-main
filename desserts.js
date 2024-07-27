ecce510c5af517d746207f7a385efe5fe199e86b
Waris Fayiz <mohammadwaris456@gmail.com>
2024-07-28 01:08:47 +0330

    HTML Coded

---
+++ desserts.js
@@ -0,0 +1,85 @@
+const desserts = [
+    {
+        name: 'waffel',
+        description: 'waffle with Berries',
+        cost: 6.5,
+        imageMobile: 'image-waffle-mobile.jpg',
+        imageTablet: 'image-waffle-tablet.jpg',
+        imageDesktop: 'image-waffle-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Crème Brûlée',
+        description: 'Vanilla Bean Crème Brûlée',
+        cost: 7.00,
+        imageMobile: 'image-creme-brulee-mobile.jpg',
+        imageTablet: 'image-creme-brulee-tablet.jpg',
+        imageDesktop: 'image-creme-brulee-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Macaron',
+        description: 'Macaron Mix of Five',
+        cost: 8.00,
+        imageMobile: 'image-macaron-mobile.jpg',
+        imageTablet: 'image-macaron-tablet.jpg',
+        imageDesktop: 'image-macaron-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Tiramisu',
+        description: 'Classic Tiramisu',
+        cost: 5.50,
+        imageMobile: 'image-tiramisu-mobile.jpg',
+        imageTablet: 'image-tiramisu-tablet.jpg',
+        imageDesktop: 'image-tiramisu-desktop.jpg'
+
+    }
+    ,
+    {
+        name: 'Baklava',
+        description: 'Pistachio Baklava',
+        cost: 4.00,
+        imageMobile: 'image-baklava-mobile.jpg',
+        imageTablet: 'image-baklava-tablet.jpg',
+        imageDesktop: 'image-baklava-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Pie',
+        description: 'Lemon Meringue Pie',
+        cost: 65.00,
+        imageMobile: 'image-meringue-mobile.jpg',
+        imageTablet: 'image-meringue-tablet.jpg',
+        imageDesktop: 'image-meringue-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Cake',
+        description: 'Red Velvet Cake',
+        cost: 4.50,
+        imageMobile: 'image-cake-mobile.jpg',
+        imageTablet: 'image-cake-tablet.jpg',
+        imageDesktop: 'image-cake-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Brownie',
+        description: 'Salted Caramel Brownie',
+        cost: 4.50,
+        imageMobile: 'image-brownie-mobile.jpg',
+        imageTablet: 'image-brownie-tablet.jpg',
+        imageDesktop: 'image-brownie-desktop.jpg'
+    }
+    ,
+    {
+        name: 'Panna Cotta',
+        description: 'Vanilla Panna Cotta',
+        cost: 6.50,
+        imageMobile: 'image-panna-cotta-moblile.jpg',
+        imageTablet: 'image-panna-cotta-tablet.jpg',
+        imageDesktop: 'image-panna-cotta-desktop.jpg'
+    }
+]
+
+export default desserts 
